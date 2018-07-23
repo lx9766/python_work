0ef90d696ce20edab009e970fb5d1a441b7f9c54
lx9766 <2667313114@qq.com>
2018-07-23 23:40:01 +0800

    2018.7.23.23:39

--- magicians.py
+++ magicians.py
@@ -6,4 +6,3 @@ for magician in magicians:
     print(magician.title()+',that was a great tirck!')
     print("I can't wait to see your next trick,"+magician.title())
 print(magicians)
-print(magicians)
